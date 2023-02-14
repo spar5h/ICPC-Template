1a674b7a0225525c0f8df7fd5d4dbd8597fb9ee6
spar5h <sparshno.1@gmail.com>
2023-02-14 14:31:33 -0500

    condensation graph

--- Graphs/Strongly Connected Components.java
+++ Graphs/Strongly Connected Components.java
@@ -2,6 +2,7 @@ ArrayList<Integer>[] adj, adj_rev;
 boolean[] used;
 ArrayList<Integer> order, component;
 
+
 void dfs1(int x) {
     used[x] = true;
     for (int y : adj[x]) {
@@ -20,6 +21,12 @@ void dfs2(int x) {
             dfs2(y);
     }
 }
+
+/* -- For Condensation Graph --  */
+int[] roots;
+ArrayList<Integer> root_nodes;
+ArrayList<Integer>[] adj_scc;
+/* ----------------------------  */
 
 public void solve(FastReader s, PrintWriter w) {
     int n = s.nextInt();
@@ -47,13 +54,40 @@ public void solve(FastReader s, PrintWriter w) {
 
     Collections.reverse(order);
     Arrays.fill(used, false);
+
+    /* -- For Condensation Graph --  */
+    roots = new int[n];
+    root_nodes = new ArrayList<>();
+    adj_scc = new ArrayList[n];
+    for (int i = 0; i < n; i++)
+        adj_scc[i] = new ArrayList<>();
+    /* ----------------------------  */
+
     for (int x : order) {
         if (!used[x]) {
             dfs2(x);
 
             // ... process component
 
+            /* -- For Condensation Graph --  */
+            int root = component.get(0);
+            for (int y : component) roots[y] = root;
+            root_nodes.add(root);
+            /* ----------------------------  */
+
             component.clear();
         }
     }
+
+    /* -- For Condensation Graph --  */
+    for (int x = 0; x < n; x++) {
+        for (int y : adj[x]) {
+            int root_x = roots[x], root_y = roots[y];
+
+            if (root_x != root_y)
+                adj_scc[root_x].add(root_y);
+        }
+    }
+    /* ----------------------------  */
+
 }
